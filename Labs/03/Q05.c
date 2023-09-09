4bd4fef93b44ad95e53b4acdc01830de7dccea28
muneebsyed6698 <142868081+muneebsyed6698@users.noreply.github.com>
2023-09-09 16:09:23 +0500

    Add files via upload

---
+++ Labs/03/Q05.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int Total_Dist = 10+10+5+15;
+    int Petrol = 2;
+    printf("Total Distance Travelled:  %dKM\n",Total_Dist);
+    printf("Total Petrol Consumed is %d Liters",Total_Dist*Petrol);
+}
